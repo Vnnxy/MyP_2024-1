9aa537ae7aae2d1a7188b438645d005ab9fe72d9
Akira <akira@Akiras-MacBook-Air.local>
2023-09-25 22:16:07 -0600

    Adapter added and working

---
+++ practica3/src/CheemsListensToUncleSam.java
@@ -0,0 +1,293 @@
+import java.util.ArrayList;
+
+/**
+ * Class that represents the adapter for CheemsSoldado. (Uncle Sam is proud of
+ * you :))
+ *
+ * @author equipo.
+ */
+public class CheemsListensToUncleSam implements Soldier {
+    private CheemsSoldado cheems;
+
+    /* The id of the captain */
+    private String id;
+    /* The formation of the battalion. */
+    private String formation;
+    /* The damage points of the captain. */
+    private int dps;
+    /* The speed points of the captain. */
+    private int speed;
+    /* The defense points of the captain. */
+    private int defense;
+    /* The gear the soldier will be using. */
+    private String gear;
+    /* The list of subordinates under Cheems if applicable. */
+    private ArrayList<Soldier> subordinates;
+    /* Boolean representing if cheems is a captain */
+    private boolean isCaptain = false;
+    /* Boolean representing if cheems is a lieutenant */
+    private boolean isLieutenant = false;
+
+    /**
+     * Build a new private
+     *
+     * @param id         the id for cheems.
+     * @param cheems     the instance of cheems that we will be adapting
+     * @param captain    boolean to know if it is a captain
+     * @param lieutenant boolean to know if it is a lieutenant
+     */
+    public CheemsListensToUncleSam(CheemsSoldado cheems, boolean captain, boolean lieutenant, String id,
+            String formation) {
+        gear = "";
+        this.cheems = cheems;
+        addCheemsEquipment(this.cheems);
+        if (captain && !lieutenant)
+            isCaptain = true;
+        else if (!captain && lieutenant)
+            isLieutenant = true;
+
+        this.id = id;
+        this.formation = formation;
+    }
+
+    /**
+     * 
+     * @param cheems Instance of CheemsSoldado needed for adding the equipment he
+     *               will be taking.
+     */
+
+    private void addCheemsEquipment(CheemsSoldado cheems) {
+        Reliquia gungnir = new Arma(1, "Gungnir", 5.1, "Nordico");
+        Reliquia mjolnir = new Arma(2, "Mjolnir", 3.4, "Nordico");
+        Reliquia hermes = new Vestimenta(3, "Sandalias de Hermes", 2.2, "Griego");
+        Reliquia botas = new Vestimenta(4, "Botas de 7 leguas", 1.9, "Europeo");
+        Reliquia hades = new Armadura(5, "Casco de Hades", 3.5, "Griego");
+        Reliquia aegis = new Armadura(6, "Escudo Aegis", 2.8, "Griego");
+
+        cheems.agregarReliquia(gungnir);
+        cheems.agregarReliquia(mjolnir);
+        cheems.agregarReliquia(hermes);
+        cheems.agregarReliquia(botas);
+        cheems.agregarReliquia(hades);
+        cheems.agregarReliquia(aegis);
+    }
+
+    /**
+     * Method that returns the soldier´s battle formation.
+     * 
+     * @return String the formation
+     */
+    @Override
+    public String getFormation() {
+        return formation;
+    }
+
+    /**
+     * Method that returns the soldier´s id.
+     * 
+     * @return String the id.
+     */
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Method that returns the soldier´s damage points.
+     * 
+     * @return int damage points
+     */
+    @Override
+    public int getDp() {
+        return dps;
+    }
+
+    /**
+     * Method that sets the soldier´s damage points.
+     * 
+     * @param int dp representing damage points
+     */
+    @Override
+    public void setDp(int dp) {
+        this.dps = dp;
+    }
+
+    /**
+     * Method that returns the soldier´s speed.
+     * 
+     * @return int speed.
+     */
+    @Override
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * Method that sets the soldier´s speed points.
+     * 
+     * @param int speed representing speed points
+     */
+    @Override
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * Method that returns the soldier´s defense points.
+     * 
+     * @return int defense points
+     */
+    @Override
+    public int getDefense() {
+        return defense;
+    }
+
+    /**
+     * Method that sets the soldier´s defense points.
+     * 
+     * @param int defense representing defense points
+     */
+    @Override
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    /**
+     * Method that sets the soldier´s gear.
+     * 
+     * @param gear representing gear.
+     */
+    @Override
+    public void setGear(String gear) {
+        this.gear = gear;
+    }
+
+    /**
+     * Method that returns a String representation of the soldier´s gear.
+     * 
+     */
+    @Override
+    public String getGear() {
+        return gear;
+    }
+
+    /**
+     * Method that checks if the soldier is a captain.
+     *
+     * @return true if the soldier is a captain, false otherwise.
+     */
+    @Override
+    public boolean isCaptain() {
+        return isCaptain;
+    }
+
+    /**
+     * Method that checks if the soldier is a lieutenant.
+     *
+     * @return true if the soldier is a lieutenant, false otherwise.
+     */
+    @Override
+    public boolean isLieutenant() {
+        return isLieutenant;
+    }
+
+    /**
+     * Method that checks if the Iron Cheems has been used.
+     *
+     * @return true if Iron Cheems has been used, false otherwise.
+     */
+    @Override
+    public boolean hasIronCheemsBeenUsed() {
+        return false;
+    }
+
+    /**
+     * Method to give a report of the battalion.
+     *
+     */
+    @Override
+    public void report() {
+        System.out.println(this.toString());
+        if (isCaptain || isLieutenant) {
+            for (Soldier soldier : subordinates)
+                soldier.report();
+        }
+    }
+
+    /**
+     * Method to relay an order to the battalion under the lieutenant.
+     * 
+     * @param oder the order
+     */
+    @Override
+    public void relayOrder(String order) {
+        if (!isCaptain && !isLieutenant) {
+            System.out.println("A private can't order anything");
+            return;
+        } else if (isCaptain)
+            this.formation = order;
+        for (Soldier subordinate : subordinates)
+            subordinate.receiveOrder(order);
+
+    }
+
+    /**
+     * Method to receive an order from the lieutenant.
+     * 
+     * @param order the orde
+     */
+    @Override
+    public void receiveOrder(String order) {
+        if (isCaptain) {
+            System.out.println("A captain can't receive orders");
+            return;
+        } else if (isLieutenant) {
+            relayOrder(order);
+        }
+        this.formation = order;
+    }
+
+    /**
+     * Method to add a soldier to the battallion under the lieutenant.
+     *
+     * @param soldier the soldier to add.
+     */
+    @Override
+    public void addSoldier(Soldier soldier) {
+        if (!isCaptain && !isLieutenant) {
+            System.out.println("A Private does not have any soldiers");
+            return;
+        } else if (isCaptain && !soldier.isLieutenant()) {
+            System.out.println("A captain can only receive lieutenants");
+            return;
+        } else if (isLieutenant && soldier.isCaptain() || soldier.isLieutenant()) {
+            System.out.println("A Lieutenat only has Privates under control");
+            return;
+        }
+        subordinates.add(soldier);
+
+    }
+
+    /**
+     * Method that returns the soldier´s full report.
+     * 
+     * @return String
+     */
+    @Override
+    public String toString() {
+        String tabs = "\n\t\t";
+        StringBuffer information = new StringBuffer();
+        if (isLieutenant)
+            tabs = "\n\t";
+        else if (isCaptain)
+            tabs = "\n";
+        information.append(cheems.infoCheems());
+        information.append(tabs + "Cheems: ").append(id);
+        information.append(tabs + "Battalion formation: ").append(formation);
+        information.append(tabs + "Damage: ").append(dps);
+        information.append(tabs + "Defense: ").append(defense);
+        information.append(tabs + "Speed: ").append(speed);
+        information.append(tabs + "Gear: ").append(gear);
+        return information.toString();
+    }
+}
--- practica3/src/Main.java
+++ practica3/src/Main.java
@@ -1,230 +1,308 @@
+
 /**
  * Main class, where thr user intercats with the system
  *
  */
 import java.util.Scanner;
-public class Main{
 
-    /**
-     * Main method
-     */
-    public static void main(String[] args){
-	Soldier s1 = new Private("1");
-	Soldier s2 = new Private("2");
-	Soldier s3 = new Private("3");
-	Soldier s4 = new Private("4");
-	Soldier s5 = new Private("5");
-	Soldier s6 = new Private("6");
-	Soldier s7 = new Private("7");
-	Soldier s8 = new Private("8");
-	Soldier s9 = new Private("9");
-	Soldier s10 = new Private("10");
-	Soldier s11 = new Private("11");
-	Soldier s12 = new Private("12");
-	Soldier s13 = new Private("13");
-	Soldier s14 = new Private("14");
-	Soldier s15 = new Private("15");
-	Soldier t1 = new Lieutenant("1");
-	Soldier t2 = new Lieutenant("2");
-	Soldier t3 = new Lieutenant("3");
-	Soldier t4 = new Lieutenant("4");
-	Soldier t5 = new Lieutenant("5");
-	Soldier t6 = new Lieutenant("6");
-	t1 = new KevlarArmor(t1);
-	t4 = new GrapheneArmor(t4);
-	t6 = new DragonSlayerSword(t6);
-	Soldier c1 = new Captain("1","alpha");
-	Soldier c2 = new Captain("2","betta");
-	Soldier c3 = new Captain("3", "charlie");
-	c1 = new IronCheems(c1);
-	c2 = new LaserBeam(new TankArmor(c2));
-	t1.addSoldier(s1);
-	t1.addSoldier(s2);
-	t1.addSoldier(s3);
-	t2.addSoldier(s4);
-	t2.addSoldier(s5);
-	t3.addSoldier(s6);
-	t3.addSoldier(s7);
-	t4.addSoldier(s8);
-	t4.addSoldier(s9);
-	t4.addSoldier(s10);
-	t5.addSoldier(s11);
-	t5.addSoldier(s12);
-	t6.addSoldier(s13);
-	t6.addSoldier(s14);
-	t6.addSoldier(s15);
-	c1.addSoldier(t1);
-	c1.addSoldier(t2);
-	c2.addSoldier(t3);
-	c2.addSoldier(t4);
-	c3.addSoldier(t5);
-	c3.addSoldier(t6);
-	c1.relayOrder(c1.getFormation());
-	c2.relayOrder(c2.getFormation());
-	c3.relayOrder(c3.getFormation());
-	Scanner sc = new Scanner(System.in);
-	while(true){
-	    System.out.println("Please choose an option: ");
-	    System.out.println("1.- Equip a new Soldier.");
-	    System.out.println("2.- Report Batallions");
-	    System.out.println("0.- Quit");
-	    try{
+public class Main {
+
+	/**
+	 * Main method
+	 */
+	public static void main(String[] args) {
+		Soldier s1 = new Private("1");
+		Soldier s2 = new Private("2");
+		Soldier s3 = new Private("3");
+		Soldier s4 = new Private("4");
+		Soldier s5 = new Private("5");
+		Soldier s6 = new Private("6");
+		Soldier s7 = new Private("7");
+		Soldier s8 = new Private("8");
+		Soldier s9 = new Private("9");
+		Soldier s10 = new Private("10");
+		Soldier s11 = new Private("11");
+		Soldier s12 = new Private("12");
+		Soldier s13 = new Private("13");
+		Soldier s14 = new Private("14");
+		Soldier s15 = new Private("15");
+		Soldier t1 = new Lieutenant("1");
+		Soldier t2 = new Lieutenant("2");
+		Soldier t3 = new Lieutenant("3");
+		Soldier t4 = new Lieutenant("4");
+		Soldier t5 = new Lieutenant("5");
+		Soldier t6 = new Lieutenant("6");
+		CheemsSoldado cheemsSoldier = new CheemsSoldado("Cheems", 444);
+		Soldier cheems = new CheemsListensToUncleSam(cheemsSoldier, false, false, "444", "Get up");
+		t1 = new KevlarArmor(t1);
+		t4 = new GrapheneArmor(t4);
+		t6 = new DragonSlayerSword(t6);
+		Soldier c1 = new Captain("1", "alpha");
+		Soldier c2 = new Captain("2", "betta");
+		Soldier c3 = new Captain("3", "charlie");
+		c1 = new IronCheems(c1);
+		c2 = new LaserBeam(new TankArmor(c2));
+		t1.addSoldier(s1);
+		t1.addSoldier(s2);
+		t1.addSoldier(s3);
+		t2.addSoldier(s4);
+		t2.addSoldier(s5);
+		t3.addSoldier(s6);
+		t3.addSoldier(s7);
+		t4.addSoldier(s8);
+		t4.addSoldier(s9);
+		t4.addSoldier(s10);
+		t5.addSoldier(s11);
+		t5.addSoldier(s12);
+		t6.addSoldier(s13);
+		t6.addSoldier(s14);
+		t6.addSoldier(s15);
+		c1.addSoldier(t1);
+		t1.addSoldier(cheems);
+		c1.addSoldier(t2);
+		c2.addSoldier(t3);
+		c2.addSoldier(t4);
+		c3.addSoldier(t5);
+		c3.addSoldier(t6);
+		c1.relayOrder(c1.getFormation());
+		c2.relayOrder(c2.getFormation());
+		c3.relayOrder(c3.getFormation());
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Please choose an option: ");
+			System.out.println("1.- Equip a new Soldier.");
+			System.out.println("2.- Report Batallions");
+			System.out.println("3.- Equip Cheems");
+			System.out.println("0.- Quit");
+			try {
+				String input = sc.nextLine();
+				int option = Integer.parseInt(input);
+				switch (option) {
+					case 1:
+						buildNewSoldier(sc);
+						break;
+					case 2:
+						reportBatallions(c1, c2, c3, s1, sc, cheems);
+						break;
+					case 3:
+						cheems = equipCheems(sc, cheems);
+						break;
+					case 0:
+						sc.close();
+						return;
+					default:
+						System.out.println("Please write a valid option");
+						continue;
+				}
+			} catch (NumberFormatException nfe) {
+				System.out.println("Please write a valid option");
+				continue;
+			}
+		}
+	}
+
+	/* Private method to display the soldier menu */
+	private static void buildNewSoldier(Scanner sc) {
+		System.out.println("Please write the soldier's id: ");
 		String input = sc.nextLine();
-		int option = Integer.parseInt(input);
-		switch (option){
-		case 1:
-		    buildNewSoldier(sc);
-		    break;
-		case 2:
-		    reportBatallions(c1,c2,c3,s1,sc);
-		    break;
-		case 0:
-		    sc.close();
-		    return;
-		default:
-		    System.out.println("Please write a valid option");
-		    continue;
+		Soldier soldier;
+		while (true) {
+			System.out.println("Select an option for your new soldier: ");
+			System.out.println("1.- Private");
+			System.out.println("2.- Lieutenant");
+			System.out.println("3.- Captain");
+			try {
+				String s = sc.nextLine();
+				int option = Integer.parseInt(s);
+				switch (option) {
+					case 1:
+						soldier = new Private(input);
+						break;
+					case 2:
+						soldier = new Lieutenant(input);
+						break;
+					case 3:
+						System.out.println("please write the battle formation: ");
+						String formation = sc.nextLine();
+						soldier = new Captain(input, formation);
+						break;
+					default:
+						System.out.println("Please write a valid option");
+						continue;
+				}
+				break;
+			} catch (NumberFormatException nfe) {
+				System.out.println("Please write a valid option");
+				continue;
+			}
 		}
-	    } catch (NumberFormatException nfe){
-		System.out.println("Please write a valid option");
-		continue;
-	    }
+		while (true) {
+			System.out.println("Your soldier status: ");
+			System.out.println(soldier.toString());
+			System.out.println("Please choose a gear for your soldier: ");
+			System.out.println("1.- Kevlar Armor");
+			System.out.println("2.- Graphene Armor");
+			System.out.println("3.- Tank Armor");
+			System.out.println("4.- Dragon Slayer Sword");
+			System.out.println("5.- Potato Gun");
+			System.out.println("6.- Laser Beam");
+			System.out.println("7.- Chicken Wings");
+			System.out.println("8.- Giant Frog Legs");
+			System.out.println("9.- Turbine");
+			System.out.println("10.- Iron Cheems");
+			System.out.println("0.- Quit");
+			try {
+				String ans = sc.nextLine();
+				int opt = Integer.parseInt(ans);
+				switch (opt) {
+					case 1:
+						soldier = new KevlarArmor(soldier);
+						break;
+					case 2:
+						soldier = new GrapheneArmor(soldier);
+						break;
+					case 3:
+						soldier = new TankArmor(soldier);
+						break;
+					case 4:
+						soldier = new DragonSlayerSword(soldier);
+						break;
+					case 5:
+						soldier = new PotatoGun(soldier);
+						break;
+					case 6:
+						soldier = new LaserBeam(soldier);
+						break;
+					case 7:
+						soldier = new ChickenWings(soldier);
+						break;
+					case 8:
+						soldier = new GiantFrogLegs(soldier);
+						break;
+					case 9:
+						soldier = new Turbine(soldier);
+						break;
+					case 10:
+						soldier = new IronCheems(soldier);
+						break;
+					case 0:
+						return;
+					default:
+						System.out.println("Please write a valid option");
+						continue;
+				}
+			} catch (NumberFormatException nfe) {
+				System.out.println("Please write a valid option");
+				continue;
+			}
+		}
 	}
-    }
 
-    /*Private method to display the soldier menu*/
-    private static void buildNewSoldier(Scanner sc){
-	System.out.println("Please write the soldier's id: ");
-	String input = sc.nextLine();
-	Soldier soldier;
-	while(true){
-	    System.out.println("Select an option for your new soldier: ");
-	    System.out.println("1.- Private");
-	    System.out.println("2.- Lieutenant");
-	    System.out.println("3.- Captain");
-	    try {
-		String s = sc.nextLine();
-		int option = Integer.parseInt(s);
-		switch (option){
-		case 1:
-		    soldier = new Private(input);
-		    break;
-		case 2:
-		    soldier = new Lieutenant(input);
-		    break;
-		case 3:
-		    System.out.println("please write the battle formation: ");
-		    String formation = sc.nextLine();
-		    soldier = new Captain(input, formation);
-		    break;
-		default:
-		    System.out.println("Please write a valid option");
-		    continue;
-		}
-		break;
-	    } catch (NumberFormatException nfe){
-		System.out.println("Please write a valid option");
-		continue;
-	    }
-	}
-	while(true){
-	    System.out.println("Your soldier status: ");
-	    System.out.println(soldier.toString());
-	    System.out.println("Please choose a gear for your soldier: ");
-	    System.out.println("1.- Kevlar Armor");
-	    System.out.println("2.- Graphene Armor");
-	    System.out.println("3.- Tank Armor");
-	    System.out.println("4.- Dragon Slayer Sword");
-	    System.out.println("5.- Potato Gun");
-	    System.out.println("6.- Laser Beam");
-	    System.out.println("7.- Chicken Wings");
-	    System.out.println("8.- Giant Frog Legs");
-	    System.out.println("9.- Turbine");
-	    System.out.println("10.- Iron Cheems");
-	    System.out.println("0.- Quit");
-	    try {
-		String ans = sc.nextLine();
-		int opt = Integer.parseInt(ans);
-		switch (opt){
-		case 1:
-		    soldier = new KevlarArmor(soldier);
-		    break;
-		case 2:
-		    soldier = new GrapheneArmor(soldier);
-		    break;
-		case 3:
-		    soldier = new TankArmor(soldier);
-		    break;
-		case 4:
-		    soldier = new DragonSlayerSword(soldier);
-		    break;
-		case 5:
-		    soldier = new PotatoGun(soldier);
-		    break;
-		case 6:
-		    soldier = new LaserBeam(soldier);
-		    break;
-		case 7:
-		    soldier = new ChickenWings(soldier);
-		    break;
-		case 8:
-		    soldier = new GiantFrogLegs(soldier);
-		    break;
-		case 9:
-		    soldier = new Turbine(soldier);
-		    break;
-		case 10:
-		    soldier = new IronCheems(soldier);
-		    break;
-		case 0:
-		    return;
-		default:
-		    System.out.println("Please write a valid option");
-		    continue;
-		}
-	    } catch (NumberFormatException nfe){
-		System.out.println("Please write a valid option");
-		continue;
-	    }
-	}
-    }
+	/* Private method to display the cheems menu */
+	private static Soldier equipCheems(Scanner sc, Soldier cheemsSoldier) {
+		Soldier soldier = cheemsSoldier;
 
-    /*Private methos to display the batallions menu*/
-    private static void reportBatallions(Soldier c1, Soldier c2, Soldier c3, Soldier s1, Scanner sc){
-	while (true){
-	    System.out.println("Please choose an option");
-	    System.out.println("1.- Complete Report");
-	    System.out.println("2.- Second Batallion Report");
-	    System.out.println("3.- One Private Soldier Report");
-	    System.out.println("0.- Quit");
-	    try {
-		String input = sc.nextLine();
-		int option = Integer.parseInt(input);
-		switch (option){
-		case 1:
-		    c1.report();
-		    c2.report();
-		    c3.report();
-		    break;
-		case 2:
-		    c2.report();
-		    break;
-		case 3:
-		    s1.report();
-		    break;
-		case 0:
-		    return;
-		default:
-		    System.out.println("Please write a valid option");
-		    continue;
+		while (true) {
+			System.out.println("Your cheems status: ");
+			System.out.println(soldier.toString());
+			System.out.println("Please choose a gear for Cheems: ");
+			System.out.println("1.- Kevlar Armor");
+			System.out.println("2.- Graphene Armor");
+			System.out.println("3.- Tank Armor");
+			System.out.println("4.- Dragon Slayer Sword");
+			System.out.println("5.- Potato Gun");
+			System.out.println("6.- Laser Beam");
+			System.out.println("7.- Chicken Wings");
+			System.out.println("8.- Giant Frog Legs");
+			System.out.println("9.- Turbine");
+			System.out.println("10.- Iron Cheems");
+			System.out.println("0.- Quit");
+			try {
+				String ans = sc.nextLine();
+				int opt = Integer.parseInt(ans);
+				switch (opt) {
+					case 1:
+						soldier = new KevlarArmor(soldier);
+						break;
+					case 2:
+						soldier = new GrapheneArmor(soldier);
+						break;
+					case 3:
+						soldier = new TankArmor(soldier);
+						break;
+					case 4:
+						soldier = new DragonSlayerSword(soldier);
+						break;
+					case 5:
+						soldier = new PotatoGun(soldier);
+						break;
+					case 6:
+						soldier = new LaserBeam(soldier);
+						break;
+					case 7:
+						soldier = new ChickenWings(soldier);
+						break;
+					case 8:
+						soldier = new GiantFrogLegs(soldier);
+						break;
+					case 9:
+						soldier = new Turbine(soldier);
+						break;
+					case 10:
+						soldier = new IronCheems(soldier);
+						break;
+					case 0:
+						return soldier;
+					default:
+						System.out.println("Please write a valid option");
+						continue;
+				}
+			} catch (NumberFormatException nfe) {
+				System.out.println("Please write a valid option");
+				continue;
+			}
 		}
-		break;
-	    } catch (NumberFormatException nfe){
-		System.out.println("Please write a valid option");
-		continue;
-	    }
 	}
-    }
 
-    
+	/* Private methos to display the batallions menu */
+	private static void reportBatallions(Soldier c1, Soldier c2, Soldier c3, Soldier s1, Scanner sc, Soldier cheems) {
+		while (true) {
+			System.out.println("Please choose an option");
+			System.out.println("1.- Complete Report");
+			System.out.println("2.- Second Batallion Report");
+			System.out.println("3.- One Private Soldier Report");
+			System.out.println("4.- Cheems Report");
+			System.out.println("0.- Quit");
+			try {
+				String input = sc.nextLine();
+				int option = Integer.parseInt(input);
+				switch (option) {
+					case 1:
+						c1.report();
+						c2.report();
+						c3.report();
+						break;
+					case 2:
+						c2.report();
+						break;
+					case 3:
+						s1.report();
+						break;
+					case 4:
+						cheems.report();
+						break;
+					case 0:
+						return;
+					default:
+						System.out.println("Please write a valid option");
+						continue;
+				}
+				break;
+			} catch (NumberFormatException nfe) {
+				System.out.println("Please write a valid option");
+				continue;
+			}
+		}
+	}
+
 }
