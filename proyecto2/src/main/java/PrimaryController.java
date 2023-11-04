40738cb6a7a8665689f45761159f1613df3eb221
Htska <112268934+Htska@users.noreply.github.com>
2023-11-03 19:49:33 -0600

    Primary window fxml added, controller skeleton added

---
+++ proyecto2/src/main/java/PrimaryController.java
@@ -0,0 +1,62 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+public class PrimaryController {
+
+    @FXML
+    private MenuItem changeItem;
+
+    @FXML
+    private MenuItem connectItem;
+
+    @FXML
+    private Menu connectMenu;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Menu exitMenu;
+
+    @FXML
+    private Menu languageMenu;
+
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    private MenuItem logoutItem;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void changeLanguage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void connect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewAndEdit(ActionEvent event) {
+
+    }
+
+}
