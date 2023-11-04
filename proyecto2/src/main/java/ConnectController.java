863c524c2e6c3aaf52eb283bc390da4b99037511
Htska <112268934+Htska@users.noreply.github.com>
2023-11-03 21:12:03 -0600

    menu windows added with their skeleton controller

---
+++ proyecto2/src/main/java/ConnectController.java
@@ -0,0 +1,37 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ConnectController {
+
+    @FXML
+    private Label addressLabel;
+
+    @FXML
+    private TextField addressText;
+
+    @FXML
+    private Button connectButton;
+
+    @FXML
+    private Label portLabel;
+
+    @FXML
+    private TextField portText;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void connect(ActionEvent event) {
+
+    }
+
+}
---
+++ proyecto2/src/main/java/LanguageController.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class LanguageController {
+
+    @FXML
+    private Button languageButton;
+
+    @FXML
+    private ChoiceBox<String> languageChoice;
+
+    @FXML
+    private Label languageLabel;
+
+    @FXML
+    void accept(ActionEvent event) {
+
+    }
+
+}
