93a69e84e2a59c8bdeafa96dbb60ffd98538c8e7
Htska <112268934+Htska@users.noreply.github.com>
2023-11-04 19:04:05 -0600

    Addesd the final fxml and the skeleton controllers

---
+++ proyecto2/src/main/java/ListController.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class ListController {
+
+    @FXML
+    private Button fileButton;
+
+    @FXML
+    private ListView<String> filesList;
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    void fileSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getBack(ActionEvent event) {
+
+    }
+
+}
---
+++ proyecto2/src/main/java/TextEditorController.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TextEditorController {
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField textSpace;
+
+    @FXML
+    void edit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+}
