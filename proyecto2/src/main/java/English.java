fffd7100cd3ae5d2a6ca8fc1a6ef919cc3c07d1a
Htska <112268934+Htska@users.noreply.github.com>
2023-11-04 20:40:36 -0600

    Surprise added

---
+++ proyecto2/src/main/java/English.java
@@ -0,0 +1,189 @@
+/**
+ * Class that represents the english language of the gui.
+ * 
+ * @author equipo.
+ */
+
+public class English implements Language {
+
+    /**
+     * Message for change language menu item
+     * 
+     * @return the message
+     */
+    public String changeItem(){
+	return "Change Language";
+    }
+
+    /**
+     * Message for connect  menu item
+     * 
+     * @return the message
+     */
+    public String connectItem(){
+	return "Connect to server";
+    }
+
+    /**
+     * Message for connect menu
+     * 
+     * @return the message
+     */
+    public String connectMenu(){
+	return "Connect";
+    }
+
+    /**
+     * Message for the view and edit button
+     * 
+     * @return the message
+     */
+    public String editButton(){
+	return "View/Edit";
+    }
+
+    /**
+     * Message for the exit menu
+     * 
+     * @return the message
+     */
+    public String exitMenu(){
+	return "Exit";
+    }
+
+    /**
+     * Message for the language menu
+     * 
+     * @return the message
+     */
+    public String languageMenu(){
+	return "Language";
+    }
+
+    /**
+     * Message for the load button
+     * 
+     * @return the message
+     */
+    public String loadButton(){
+	return "Load File";
+    }
+
+    /**
+     * Message for logout  menu item
+     * 
+     * @return the message
+     */
+    public String logoutItem(){
+	return "Logout";
+    }
+
+    /**
+     * Message for the welcome label
+     * 
+     * @return the message
+     */
+    public String welcomeText(){
+	return "Welcome!!";
+    }
+
+    /**
+     * Message for the address label
+     * 
+     * @return the message
+     */
+    public String addressLabel(){
+	return "Address:";
+    }
+
+    /**
+     * Message for the connect button
+     * 
+     * @return the message
+     */
+    public String connectButton(){
+	return "Connect";
+    }
+
+    /**
+     * Message for the port label
+     * 
+     * @return the message
+     */
+    public String portLabel(){
+	return "Port:";
+    }
+    
+    /**
+     * Message for the return button
+     * 
+     * @return the message
+     */
+    public String returnButton(){
+	return "Return";
+    }
+
+    /**
+     * Message for the language accept button 
+     * 
+     * @return the message
+     */
+    public String languageButton(){
+	return "Accept";
+    }
+
+    /**
+     * Message for the language label
+     * 
+     * @return the message
+     */
+    public String languageLabel(){
+	return "Choose a Language";
+    }
+
+    /**
+     * Message to accept a file button
+     * 
+     * @return the message
+     */
+    public String fileButton(){
+	return "Accept";
+    }
+
+    /**
+     * Message to change language info label
+     * 
+     * @return the message
+     */
+    public String infoLabel(){
+	return "Please choose a file to view or edit";
+    }
+
+    /**
+     * Message to return from language menu 
+     * 
+     * @return the message
+     */
+    public String returnLButton(){
+	return "Cancel";
+    }
+
+    /**
+     * Message for the edit button in the text editor
+     * 
+     * @return the message
+     */
+    public String editTButton(){
+	return "Edit";
+    }
+
+    /**
+     * Message for the save button
+     * 
+     * @return the message
+     */
+    public String saveButton(){
+	return "Save";
+    }
+    
+}
---
+++ proyecto2/src/main/java/Language.java
@@ -0,0 +1,149 @@
+/**
+ * Interface that represents the language of the gui.
+ * 
+ * @author equipo.
+ */
+
+public interface Language {
+
+    /**
+     * Message for change language menu item
+     * 
+     * @return the message
+     */
+    public String changeItem();
+
+    /**
+     * Message for connect  menu item
+     * 
+     * @return the message
+     */
+    public String connectItem();
+
+    /**
+     * Message for connect menu
+     * 
+     * @return the message
+     */
+    public String connectMenu();
+
+    /**
+     * Message for the view and edit button
+     * 
+     * @return the message
+     */
+    public String editButton();
+
+    /**
+     * Message for the exit menu
+     * 
+     * @return the message
+     */
+    public String exitMenu();
+
+    /**
+     * Message for the language menu
+     * 
+     * @return the message
+     */
+    public String languageMenu();
+
+    /**
+     * Message for the load button
+     * 
+     * @return the message
+     */
+    public String loadButton();
+
+    /**
+     * Message for logout  menu item
+     * 
+     * @return the message
+     */
+    public String logoutItem();
+
+    /**
+     * Message for the welcome label
+     * 
+     * @return the message
+     */
+    public String welcomeText();
+
+    /**
+     * Message for the address label
+     * 
+     * @return the message
+     */
+    public String addressLabel();
+
+    /**
+     * Message for the connect button
+     * 
+     * @return the message
+     */
+    public String connectButton();
+
+    /**
+     * Message for the port label
+     * 
+     * @return the message
+     */
+    public String portLabel();
+
+    /**
+     * Message for the return button
+     * 
+     * @return the message
+     */
+    public String returnButton();
+
+    /**
+     * Message for the language accept button 
+     * 
+     * @return the message
+     */
+    public String languageButton();
+
+    /**
+     * Message for the language label
+     * 
+     * @return the message
+     */
+    public String languageLabel();
+
+    /**
+     * Message to accept a file button
+     * 
+     * @return the message
+     */
+    public String fileButton();
+
+    /**
+     * Message to change language info label
+     * 
+     * @return the message
+     */
+    public String infoLabel();
+
+    /**
+     * Message to return from language menu 
+     * 
+     * @return the message
+     */
+    public String returnLButton();
+
+    /**
+     * Message for the edit button in the text editor
+     * 
+     * @return the message
+     */
+    public String editTButton();
+
+    /**
+     * Message for the save button
+     * 
+     * @return the message
+     */
+    public String saveButton();
+    
+}
