773dbec0d61fc32a0f64a75aa744a26e912b68e7
Htska <112268934+Htska@users.noreply.github.com>
2023-11-05 17:03:13 -0600

    test client added

---
+++ proyecto2/src/main/java/Client.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Set;
+import java.io.*;
+
+public class Client {
+
+    private HashMap<String,String> files;
+    private int c;
+
+    public Client(){
+	files = new HashMap<String,String>();
+	files.put("file1.txt","this is file 1");
+	files.put("file2.txt","this is file 2");
+	files.put("file3.txt","this is file 3");
+    }
+
+    public ArrayList<String> getFiles(){
+	return new ArrayList<String>(files.keySet());
+    }
+
+    public void addFile(File a){
+	String n = String.format("newfile%d.txt",++c);
+	String content = String.format("this is new file %d",c);
+	files.put(n,content);
+    }
+
+    public String getContent(String s){
+	return files.get(s);
+    }
+
+    public void editFile(String n, String v){
+	files.put(n,v);
+    }
+}
